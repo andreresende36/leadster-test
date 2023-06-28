da9ee7b99a6c6f5a85ecbfa27d637997a27ccd0e
André Resende <andreresende36@gmail.com>
2023-06-28 16:15:39 -0300

    Implementação do FilterContext

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,7 +1,7 @@
 import { useFilterContext } from "@/context/FilterContext";
 import { useEffect, useState } from "react";
 
-function FilterButton({text}: {text: string}) {
+function FilterButton({ text }: { text: string }) {
   const { selectedFilter, setSelectedFilter } = useFilterContext();
   const [active, setActive] = useState(false);
 
@@ -12,19 +12,24 @@ function FilterButton({text}: {text: string}) {
   const handleClick = () => {
     setActive(true);
     setSelectedFilter(text);
-  }
+  };
 
-  return <button 
-    className={`
+  return (
+    <button
+      className={`
       btn normal-case rounded-full px-5 py-2 w-fit h-fit whitespace-nowrap transition duration-200
       border-[0.1rem] font-semibold text-[0.98rem]
-      ${ active 
-        ? 'bg-[#007EFF] border-[#007EFF] font-extrabold text-[#FFFFFF] hover:bg-[#007EFF] hover:text-[#FFFFFF] hover:border-[#007EFF]' 
-        : 'hover:border-[#007EFF] hover:text-[#007EFF] border-[#21465e]'}
+      ${
+        active
+          ? "bg-[#007EFF] border-[#007EFF] font-extrabold text-[#FFFFFF] hover:bg-[#007EFF] hover:text-[#FFFFFF] hover:border-[#007EFF]"
+          : "hover:border-[#007EFF] hover:text-[#007EFF] border-[#21465e]"
+      }
     `}
-    onClick={handleClick}>
+      onClick={handleClick}
+    >
       {text}
-    </button>;
+    </button>
+  );
 }
 
 export default FilterButton;
