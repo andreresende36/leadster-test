a4eec3f05a0de6b5d53ecacb5a3bd28354472afe
André Resende <andreresende36@gmail.com>
2023-06-29 11:40:08 -0300

    Pequenas refatorações em Filters.tsx

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -4,17 +4,16 @@ import { useEffect, useState } from "react";
 import { Filters } from "@/types/filterTypes";
 
 function Filters() {
-
   const [categories, setCategories] = useState<Filters[]>([]);
 
   useEffect(() => {
-    const set = new Set();
-    videoData.forEach((video) => set.add(video.category));
-    set.add('Todos');
-    setCategories(Array.from(set).sort() as Filters[]);
+    const uniqueCategories = Array.from(new Set(videoData.map((video) => video.category)));
+    const sortedCategories = [...uniqueCategories, 'Todos'].sort() as Filters[];
+    setCategories(sortedCategories);
   }, []);
+
   return (
-    <div className='w-[70%] flex gap-5 flex-wrap'>
+    <div className="w-[70%] flex gap-5 flex-wrap">
       {categories.map((category) => (
         <FilterButton text={category} key={category} />
       ))}
