04a7e115a077bc416d40cbf20853027aa885cd7a
André Resende <andreresende36@gmail.com>
2023-06-29 12:21:11 -0300

    Pequenas refatorações em Stats.tsx

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 import { links } from "../../public/links";
 
 function Stats() {
+  const styleButton = 'btn rounded-full bg-[#0083FF] border-0 text-white font-semibold px-8 py-[1.5rem] w-fit h-fit shadow-md hover:bg-[#0077e7]'
   return (
     <section className="bg-[#F0F8FF] h-[735px] w-[100%] flex justify-center items-center gap-10 text-[#15364A]">
       <div className="w-[50%] flex justify-end items-center ">
         <Image
-          src="/../public/comparativo.png"
+          src="/comparativo.png"
           alt="Comparativo captações de leads"
           width={739}
           height={687}
@@ -18,8 +19,7 @@ function Stats() {
       <div className="w-[50%] flex flex-col justify-start items-center">
         <div className="w-full flex flex-col gap-4">
           <p className="text-4xl font-medium">
-            Pronto para triplicar sua
-            <br></br>
+            Pronto para triplicar sua <br />
             <span className="font-bold">Geração de Leads?</span>
           </p>
           <p className="w-fit text-xl font-medium pb-5 pr-48 border-b-2 border-[#15364a2c]">
@@ -29,20 +29,10 @@ function Stats() {
 
         <div className="w-full flex items-center mt-4 gap-3">
           <Link href={links.demonstracao}>
-            <button
-              className={`
-              btn rounded-full bg-[#0083FF] border-0 
-              text-white font-semibold 
-              px-8 py-[1.5rem] w-fit h-fit
-              shadow-md
-              hover:bg-[#0077e7]
-              `}
-            >
-              Ver Demonstração
-            </button>
+            <button className={styleButton}>Ver Demonstração</button>
           </Link>
           <Image
-            src="/../public/selo_RD.png"
+            src="/selo_RD.png"
             alt="Selo RD Station"
             width={205}
             height={73}
@@ -59,11 +49,10 @@ function Stats() {
             className="w-fit h-fit"
           />
           <p className="font-medium pr-3 border-r-2 border-[#15364a98]">
-            <span className="font-bold">Não</span> é necessário Cartão de
-            Crédito
+            <span className="font-bold">Não</span> é necessário Cartão de Crédito
           </p>
           <Image
-            src="/../public/rating.webp"
+            src="/rating.webp"
             alt="Nota de avaliação"
             width={92}
             height={16}
