1e0de9f9ce0bf7f59e6fc0d170cb8a1e74b338d8
André Resende <andreresende36@gmail.com>
2023-07-03 17:59:19 -0300

    Fix title tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,25 +7,32 @@ import { VideoProvider } from "@/context/VideoContext";
 import { FilterProvider } from "@/context/FilterContext";
 import { SortProvider } from "@/context/SortContext";
 import VideosSection from "@/components/VideosSection";
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 export default function Home() {
   return (
-    <>
+    <div>
       <VideoProvider>
         <PageProvider>
           <FilterProvider>
             <SortProvider>
-              <Header />
-              <Banner />
-              <VideosSection />
-              <Stats />
+              <Head>
+                <title>
+                  Leadster: Chatbot de Marketing para Aumentar Geração de Leads
+                </title>
+              </Head>
+              <main>
+                <Header />
+                <Banner />
+                <VideosSection />
+                <Stats />
+              </main>
               <Footer />
             </SortProvider>
           </FilterProvider>
         </PageProvider>
       </VideoProvider>
       <Analytics />
-    </>
+    </div>
   );
 }
